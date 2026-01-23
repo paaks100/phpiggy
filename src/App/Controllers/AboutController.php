45894c1b7e4889d8be9355afba3dcaf058eb371d
paaks100 <pkkodua@gmail.com>
2026-01-23 12:52:31 +0000

    template engine

---
+++ src/App/Controllers/AboutController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Config\Paths;
+use Framework\TemplateEngine;
+
+class AboutController
+{
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
+
+    public function about()
+    {
+        echo $this->view->render("/about.php", [
+            "title" => "About page",
+            "dangerousData" => "<script>alert(123)</script>"
+        ]);
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,6 +6,10 @@ require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 
+use function App\Config\registerRoutes;
+
 $app = new App;
+
+registerRoutes($app);
 
 return $app;
